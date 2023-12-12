ef9c996d4c0f589a999ffaae1eb3600105ea64f8
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-12-12 20:51:32 +0500

    add the next section

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 import Image from 'next/image'
 import Hero from './components/Hero/Hero'
 import Newtabs from './components/Newtabs/Newtabs'
-// import Findtrend from './components/Findtrend/Findtrend'
-// import Partner from './components/Partner/Partner'
-// import Platform from './components/Platform/Platform'
-// import Subscription from './components/Subscription/Subscription'
-// import Joinemail from './components/Joinemail/Joinemail'
+
 
 export default function Home() {
   return (
     <main>
     <Hero/>
     <Newtabs/>
-      {/* <Findtrend/> */}
-      {/* <Partner/>
-      <Platform/>
-      <Subscription/>
-      <Joinemail/>  */}
   </main>
   )
 }
