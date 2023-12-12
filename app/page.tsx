e94ae57aad84d6e9e7b6c492222a6dbafbcbe7d5
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-12-12 21:09:44 +0500

    add the subscription section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Newtabs from './components/Newtabs/Newtabs'
 import Findtrend from './components/Findtrend/Findtrend'
 import Partner from './components/Partner/Partner'
 import Platform from './components/Platform/Platform'
+import Subscription from './components/Subscription/Subscription'
 
 
 
@@ -16,6 +17,7 @@ export default function Home() {
     <Findtrend/>
     <Partner/>
     <Platform/>
+    <Subscription/>
 
 
     
